67b4e1598e1c4ee3878f787b90b95e0d646f2276
Tommy Nguyen <khenchaynguyen@gmail.com>
2023-12-21 02:27:01 +0700

    up faqs

--- Models/Faq.cs
+++ Models/Faq.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace BE.Models;
 
 public partial class Faq
 {
     public int Id { get; set; }
-
+    [DisplayName("Question")]
     public string? Title { get; set; }
-
+    
     public string? Answer { get; set; }
 }
